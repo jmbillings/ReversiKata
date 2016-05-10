ef5f110716dbd2b524a8de762dc3fe9811c0a6f8
James Billings <james.billings@red-gate.com>
2016-05-10 12:08:15 +0100

    Add Green test for default board layout

--- ReversiKata.Tests/ReversiTests.cs
+++ ReversiKata.Tests/ReversiTests.cs
@@ -10,5 +10,23 @@ namespace ReversiKata.Tests
     [TestFixture]
     public class ReversiTests
     {
+
+        [Test]
+        public void NewGameBoardHasDefaultPieces()
+        {
+            var reversiBoard = new ReversiBoard();
+            for(var rowIndex = 0; rowIndex < 8; rowIndex++)
+            {
+                for (var colIndex = 0; colIndex < 8; colIndex++)
+                {
+                    if ((rowIndex == 3 && colIndex == 3) || (rowIndex == 4 && colIndex == 4))
+                        Assert.AreEqual(CellState.Black, reversiBoard.m_Cells[rowIndex, colIndex]);
+                    else if ((rowIndex == 3 && colIndex == 4) || (rowIndex == 4 && colIndex == 3))
+                        Assert.AreEqual(CellState.White, reversiBoard.m_Cells[rowIndex, colIndex]);
+                    else
+                        Assert.AreEqual(CellState.Empty, reversiBoard.m_Cells[rowIndex, colIndex]);
+                }     
+            }
+        }
     }
 }
