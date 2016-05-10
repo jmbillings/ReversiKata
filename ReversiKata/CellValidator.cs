6733d5a3b9e1c9d8bce71fb731071e2d962fcf39
jmbillings <james@puffle.co.uk>
2016-05-10 18:18:07 +0100

    Add more methods to cellValidator

--- ReversiKata/CellValidator.cs
+++ ReversiKata/CellValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReversiKata
 {
@@ -19,18 +20,67 @@ namespace ReversiKata
 
         private bool IsCellValidHorizontally(ReversiGridCoordinate coordinate, ConsoleColor color)
         {
+            var eastCells = CellsToCheck(coordinate, Direction.East);
+            var westCells = CellsToCheck(coordinate, Direction.West);
             return false;
         }
 
         private bool IsCellValidVertically(ReversiGridCoordinate coordinate, ConsoleColor color)
         {
+            var northCells = CellsToCheck(coordinate, Direction.North);
+            var southCells = CellsToCheck(coordinate, Direction.South);
             return false;
         }
 
         private bool IsCellValidDiagnoally(ReversiGridCoordinate coordinate, ConsoleColor color)
         {
+            var northEastCells = CellsToCheck(coordinate, Direction.NorthEast);
+            var southEastCells = CellsToCheck(coordinate, Direction.SouthEast);
+            var southWestCells = CellsToCheck(coordinate, Direction.SouthWest);
+            var northWestCells = CellsToCheck(coordinate, Direction.NorthWest);
             return false;
         }
 
+        /// <summary>
+        /// Gets all the cells on the board from the candidate placement until the edge of the board is reached
+        /// </summary>
+        /// <param name="startPoint">Candidate cell we're checking validity for</param>
+        /// <param name="direction">Compass direction to move on the board for cell retrieval</param>
+        /// <returns></returns>
+        private List<ReversiGridCoordinate> CellsToCheck(ReversiGridCoordinate startPoint, Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.North:
+                    return CellsToCheck(startPoint, -1, 0);
+                case Direction.NorthEast:
+                    return CellsToCheck(startPoint, -1, 1);
+                case Direction.East:
+                    return CellsToCheck(startPoint, 0, 1);
+                case Direction.SouthEast:
+                    return CellsToCheck(startPoint, 1, 1);
+                case Direction.South:
+                    return CellsToCheck(startPoint, 1, 0);
+                case Direction.SouthWest:
+                    return CellsToCheck(startPoint, 1, -1);
+                case Direction.West:
+                    return CellsToCheck(startPoint, 0, -1);
+                case Direction.NorthWest:
+                    return CellsToCheck(startPoint, -1, -1);
+                default:
+                    return new List<ReversiGridCoordinate>();
+            }
+        }
+
+        private List<ReversiGridCoordinate> CellsToCheck(ReversiGridCoordinate startPoint, int rowDirection, int columnDirection)
+        {
+            var cellList = new List<ReversiGridCoordinate>();
+
+
+
+
+
+            return cellList;
+        }
     }
 }
