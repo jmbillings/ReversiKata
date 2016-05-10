3836e0fa791927dc13582a1366c7171363d06b3b
James Billings <james.billings@red-gate.com>
2016-05-10 15:08:05 +0100

    Add a testcase generator for random grid locations

---
+++ ReversiKata.Tests/ReversiTestCaseGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReversiKata.Tests
+{
+    class ReversiTestCaseGenerator
+    {
+        internal static ReversiGridCoordinate[] GetTenRandomGridCoordinates()
+        {
+            var random = new Random();
+            var pairCount = 0;
+            var generatedCoordinates = new ReversiGridCoordinate[10]; 
+            while (pairCount < 10)
+            {
+                generatedCoordinates[pairCount] = new ReversiGridCoordinate(random.Next(5), random.Next(5));
+                pairCount++;
+            }
+            return generatedCoordinates;
+        }
+    }
+}
