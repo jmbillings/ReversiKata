7d19cfa951a5942a95fb74f47628fb777b224918
jmbillings <james@puffle.co.uk>
2016-05-10 17:28:11 +0100

    Red: Create a CellValidator class which returns false by default

---
+++ ReversiKata/CellValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReversiKata
+{
+    class CellValidator
+    {
+        internal bool IsCellValid(int rowIndex, int colIndex, CellState[,] reversiGrid, ConsoleColor color)
+        {
+            return false;
+        }
+    }
+}
--- ReversiKata/ReversiBoard.cs
+++ ReversiKata/ReversiBoard.cs
@@ -6,46 +6,26 @@ namespace ReversiKata
     public class ReversiBoard
     {
         public CellState[,] m_Cells;
+        private CellValidator m_CellValidator;
 
         public ReversiBoard(bool placeDefaultPieces = true)
         {
             m_Cells = new CellState[8, 8];
+            m_CellValidator = new CellValidator();
             if (placeDefaultPieces)
                 PopulateDefaultGame();
         }
 
         public List<ReversiGridCoordinate> GetValidMoves(ConsoleColor color)
         {
-            var lastWhiteColumnIndex = 99;
-            var lastBlackColumnIndex = 99;
             var validMoveCoordinates = new List<ReversiGridCoordinate>();
+
             for (var rowIndex = 0; rowIndex < 8; rowIndex++)
             {
                 for (var colIndex = 0; colIndex < 8; colIndex++)
                 {
-                    if (m_Cells[rowIndex, colIndex] == CellState.Black)
-                    {
-                        lastBlackColumnIndex = colIndex;
-                        if ((lastWhiteColumnIndex == colIndex - 1))
-                        {
-                            if (color == ConsoleColor.White)
-                                validMoveCoordinates.Add(new ReversiGridCoordinate(rowIndex, colIndex + 1));
-                            else if (color == ConsoleColor.Black)
-                                validMoveCoordinates.Add(new ReversiGridCoordinate(rowIndex, colIndex - 2));
-                        }
-                    }
-
-                    if (m_Cells[rowIndex, colIndex] == CellState.White)
-                    {
-                        lastWhiteColumnIndex = colIndex;
-                        if (lastBlackColumnIndex == colIndex - 1)
-                        {
-                            if (color == ConsoleColor.Black)
-                                validMoveCoordinates.Add(new ReversiGridCoordinate(rowIndex, colIndex + 1));
-                            else if (color == ConsoleColor.White)
-                                validMoveCoordinates.Add(new ReversiGridCoordinate(rowIndex, colIndex - 2));
-                        }
-                    }
+                    if (m_CellValidator.IsCellValid(rowIndex, colIndex, m_Cells, color))
+                        validMoveCoordinates.Add(new ReversiGridCoordinate(rowIndex, colIndex));
                 }
             }
 
