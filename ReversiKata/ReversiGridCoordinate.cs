76c7a1c3aa2d72ea37f49b3afc3d67e9c1cab504
James Billings <james.billings@red-gate.com>
2016-05-10 15:00:32 +0100

    Add class for grid coordinates

---
+++ ReversiKata/ReversiGridCoordinate.cs
@@ -0,0 +1,14 @@
+﻿namespace ReversiKata
+{
+    public class ReversiGridCoordinate
+    {
+        public int RowIndex { get; private set; }
+        public int ColumnIndex { get; private set; }
+
+        public ReversiGridCoordinate(int rowIndex, int columnIndex)
+        {
+            RowIndex = rowIndex;
+            ColumnIndex = columnIndex;
+        }
+    }
+}
