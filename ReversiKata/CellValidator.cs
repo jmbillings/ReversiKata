308fc24416a9fe76478f7df16c8554cf8b911e2b
jmbillings <james@puffle.co.uk>
2016-05-10 17:42:30 +0100

    Add some placeholder methods for cell validity calculation

--- ReversiKata/CellValidator.cs
+++ ReversiKata/CellValidator.cs
@@ -4,9 +4,33 @@ namespace ReversiKata
 {
     class CellValidator
     {
+        private CellState[,] m_Grid;
+
         internal bool IsCellValid(int rowIndex, int colIndex, CellState[,] reversiGrid, ConsoleColor color)
+        {
+            m_Grid = reversiGrid;
+            var coordinate = new ReversiGridCoordinate(rowIndex, colIndex);
+
+            if ((IsCellValidHorizontally(coordinate, color)) || (IsCellValidVertically(coordinate, color)) || (IsCellValidDiagnoally(coordinate, color)))
+                return true;
+
+            return false;
+        }
+
+        private bool IsCellValidHorizontally(ReversiGridCoordinate coordinate, ConsoleColor color)
         {
             return false;
         }
+
+        private bool IsCellValidVertically(ReversiGridCoordinate coordinate, ConsoleColor color)
+        {
+            return false;
+        }
+
+        private bool IsCellValidDiagnoally(ReversiGridCoordinate coordinate, ConsoleColor color)
+        {
+            return false;
+        }
+
     }
 }
