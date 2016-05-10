5d997fcebb0a5a1962255a2d807773625afa2ce9
James Billings <james.billings@red-gate.com>
2016-05-10 10:52:32 +0100

    Add nunit ref to Test class

--- ReversiKata.Tests/ReversiTests.cs
+++ ReversiKata.Tests/ReversiTests.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ReversiKata.Tests
 {
+    [TestFixture]
     public class ReversiTests
     {
     }
