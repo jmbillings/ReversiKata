7fc70fdabef969cb7e76276397cb3371798a597c
James Billings <james.billings@red-gate.com>
2016-05-16 15:56:17 +0100

    Add empty PlayMove tests

---
+++ ReversiKata.Tests/PlayMoveTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ReversiKata.Tests
+{
+    [TestFixture]
+    public class PlayMoveTests
+    {
+        public void WhenACellIsPlayedTheCorrectOpponentCellsInARowSwitchColor(int startColumn, int runLength, ConsoleColor playedColor)
+        {
+            
+        }
+
+        public void WhenACellIsPlayedTheCorrectOpponentCellsInAColumnSwitchColor(int startRow, int runLength, ConsoleColor playedColor)
+        {
+
+        }
+
+        public void WhenACellIsPlayedTheCorrectOpponentCellsInTwoDirectionsSwitchColor(int startRow, int startColumn, int runLength, ConsoleColor playedColor)
+        {
+
+        }
+
+        public void WhenACellIsPlayedTheCorrectOpponentCellsInThreeDirectionsSwitchColor(int startRow, int startColumn, int runLength, ConsoleColor playedColor)
+        {
+
+        }
+    }
+}
