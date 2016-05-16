970f00ccb9ff6dc208d7821876da8521e5b33be0
James Billings <james.billings@red-gate.com>
2016-05-16 13:56:03 +0100

    Extract some methods

--- ReversiKata/Program.cs
+++ ReversiKata/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReversiKata
 {
@@ -10,12 +7,14 @@ namespace ReversiKata
     {
         private static BoardRenderer m_BoardRenderer;
         private static ReversiBoard m_ReversiBoard;
+        private static UIHelperMethods m_UiHelperMethods;
 
         static void Main(string[] args)
         {
             m_BoardRenderer = new BoardRenderer();
             m_ReversiBoard = new ReversiBoard();
             m_BoardRenderer.RenderBoard(m_ReversiBoard);
+            m_UiHelperMethods = new UIHelperMethods();
 
             while (true)
             {
@@ -36,81 +35,19 @@ namespace ReversiKata
                 return;
             }
 
-            var selectedMove = new ReversiGridCoordinate(GetRowIndex(userInput[1]), GetColumnIndex(userInput[2]));
-            var selectedColor = GetCellColorForLetter(userInput[0]);
+            var selectedMove = new ReversiGridCoordinate(m_UiHelperMethods.GetRowIndex(userInput[1]), m_UiHelperMethods.GetColumnIndex(userInput[2]));
+            var selectedColor = m_UiHelperMethods.GetCellColorForLetter(userInput[0]);
             var validMoves = m_ReversiBoard.GetValidMoves(selectedColor);
             var validMove = validMoves.Select(x => x.RowIndex == selectedMove.RowIndex && x.ColumnIndex == selectedMove.ColumnIndex);
 
             if (validMove.Contains(true))
             {
-                m_ReversiBoard.m_Cells[selectedMove.RowIndex, selectedMove.ColumnIndex] = GetCellStateForColor(selectedColor);
+                m_ReversiBoard.m_Cells[selectedMove.RowIndex, selectedMove.ColumnIndex] = m_UiHelperMethods.GetCellStateForColor(selectedColor);
                 m_BoardRenderer.RenderBoard(m_ReversiBoard);
             }
             else
             {
                 Console.WriteLine("Sorry, that is not a legal move");
-            }
-        }
-
-        private static ConsoleColor GetCellColorForLetter(string letter)
-        {
-            switch (letter)
-            {
-                case "w":
-                    return ConsoleColor.White;
-                case "b":
-                    return ConsoleColor.Black;
-            }
-
-            return ConsoleColor.Red;
-        }
-
-        private static CellState GetCellStateForColor(ConsoleColor color)
-        {
-            switch (color)
-            {
-                case ConsoleColor.White:
-                    return CellState.White;
-                case ConsoleColor.Black:
-                    return CellState.Black;
-            }
-
-            return CellState.Empty;
-        }
-
-        private static int GetColumnIndex(string column)
-        {
-            int columnIndex;
-            if (!int.TryParse(column, out columnIndex))
-            {
-                Console.WriteLine("Could not parse the column to a valid number");
-                return 99;
-            }
-            return columnIndex - 1;
-        }
-
-        private static int GetRowIndex(string row)
-        {
-            switch (row.ToLower())
-            {
-                case "a":
-                    return 0;
-                case "b":
-                    return 1;
-                case "c":
-                    return 2;
-                case "d":
-                    return 3;
-                case "e":
-                    return 4;
-                case "f":
-                    return 5;
-                case "g":
-                    return 6;
-                case "h":
-                    return 7;
-                default:
-                    return 99;
             }
         }
     }
---
+++ ReversiKata/UIHelperMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ReversiKata
+{
+    class UIHelperMethods
+    {
+        internal ConsoleColor GetCellColorForLetter(string letter)
+        {
+            switch (letter)
+            {
+                case "w":
+                    return ConsoleColor.White;
+                case "b":
+                    return ConsoleColor.Black;
+            }
+
+            return ConsoleColor.Red;
+        }
+
+        internal CellState GetCellStateForColor(ConsoleColor color)
+        {
+            switch (color)
+            {
+                case ConsoleColor.White:
+                    return CellState.White;
+                case ConsoleColor.Black:
+                    return CellState.Black;
+            }
+
+            return CellState.Empty;
+        }
+
+        internal int GetColumnIndex(string column)
+        {
+            int columnIndex;
+            if (!int.TryParse(column, out columnIndex))
+            {
+                Console.WriteLine("Could not parse the column to a valid number");
+                return 99;
+            }
+            return columnIndex - 1;
+        }
+
+        internal int GetRowIndex(string row)
+        {
+            switch (row.ToLower())
+            {
+                case "a":
+                    return 0;
+                case "b":
+                    return 1;
+                case "c":
+                    return 2;
+                case "d":
+                    return 3;
+                case "e":
+                    return 4;
+                case "f":
+                    return 5;
+                case "g":
+                    return 6;
+                case "h":
+                    return 7;
+                default:
+                    return 99;
+            }
+        }
+    }
+}
