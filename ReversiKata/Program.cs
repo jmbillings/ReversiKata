026b992178757bc87a80fc89689e21190552180f
James Billings <james.billings@red-gate.com>
2016-05-16 13:53:14 +0100

    Add methods to interpret and validate the selected move

--- ReversiKata/Program.cs
+++ ReversiKata/Program.cs
@@ -23,6 +23,94 @@ namespace ReversiKata
                 if (userInput.ToLower() == "x")
                     break;
 
+                TryMove(userInput);
+            }
+        }
+
+        private static void TryMove(string input)
+        {
+            var userInput = input.Split(',');
+            if (userInput.Length != 3)
+            {
+                Console.WriteLine("Unable to parse arguments, please try again");
+                return;
+            }
+
+            var selectedMove = new ReversiGridCoordinate(GetRowIndex(userInput[1]), GetColumnIndex(userInput[2]));
+            var selectedColor = GetCellColorForLetter(userInput[0]);
+            var validMoves = m_ReversiBoard.GetValidMoves(selectedColor);
+            var validMove = validMoves.Select(x => x.RowIndex == selectedMove.RowIndex && x.ColumnIndex == selectedMove.ColumnIndex);
+
+            if (validMove.Contains(true))
+            {
+                m_ReversiBoard.m_Cells[selectedMove.RowIndex, selectedMove.ColumnIndex] = GetCellStateForColor(selectedColor);
+                m_BoardRenderer.RenderBoard(m_ReversiBoard);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that is not a legal move");
+            }
+        }
+
+        private static ConsoleColor GetCellColorForLetter(string letter)
+        {
+            switch (letter)
+            {
+                case "w":
+                    return ConsoleColor.White;
+                case "b":
+                    return ConsoleColor.Black;
+            }
+
+            return ConsoleColor.Red;
+        }
+
+        private static CellState GetCellStateForColor(ConsoleColor color)
+        {
+            switch (color)
+            {
+                case ConsoleColor.White:
+                    return CellState.White;
+                case ConsoleColor.Black:
+                    return CellState.Black;
+            }
+
+            return CellState.Empty;
+        }
+
+        private static int GetColumnIndex(string column)
+        {
+            int columnIndex;
+            if (!int.TryParse(column, out columnIndex))
+            {
+                Console.WriteLine("Could not parse the column to a valid number");
+                return 99;
+            }
+            return columnIndex - 1;
+        }
+
+        private static int GetRowIndex(string row)
+        {
+            switch (row.ToLower())
+            {
+                case "a":
+                    return 0;
+                case "b":
+                    return 1;
+                case "c":
+                    return 2;
+                case "d":
+                    return 3;
+                case "e":
+                    return 4;
+                case "f":
+                    return 5;
+                case "g":
+                    return 6;
+                case "h":
+                    return 7;
+                default:
+                    return 99;
             }
         }
     }
