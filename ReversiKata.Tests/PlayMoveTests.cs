40b3c10e8ecdca983a658682b679accd3641f0f0
jmbillings <james@puffle.co.uk>
2016-05-16 17:45:35 +0100

    Red: Add failing test for horizontal rows changing state when piece played

--- ReversiKata.Tests/PlayMoveTests.cs
+++ ReversiKata.Tests/PlayMoveTests.cs
@@ -6,9 +6,48 @@ namespace ReversiKata.Tests
     [TestFixture]
     public class PlayMoveTests
     {
-        public void WhenACellIsPlayedTheCorrectOpponentCellsInARowSwitchColor(int startColumn, int runLength, ConsoleColor playedColor)
+        [TestCaseSource(typeof(ReversiTestCaseGenerator), nameof(ReversiTestCaseGenerator.GetBoardOfMultipleCellsHorizontally))]
+        public void WhenACellIsPlayedTheCorrectOpponentCellsInARowSwitchColor(ReversiBoard reversiBoard)
         {
-            
+            ReversiGridCoordinate startCell = null;
+            //setup
+            for (var rowIndex = 0; rowIndex < 8; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < 8; columnIndex++)
+                {
+                    if (reversiBoard.m_Cells[rowIndex, columnIndex] == CellState.White)
+                    {
+                        Console.WriteLine("Found first white cell at {0},{1}", rowIndex, columnIndex);
+                        startCell = new ReversiGridCoordinate(rowIndex, columnIndex);
+                        break;
+                    }
+                }
+            }
+
+            ReversiGridCoordinate playCell = null;
+            var currentColumn = startCell.ColumnIndex + 1;
+            while (playCell == null)
+            {
+                if (reversiBoard.m_Cells[startCell.RowIndex, currentColumn] == CellState.Empty)
+                {
+                    playCell = new ReversiGridCoordinate(startCell.RowIndex, currentColumn);
+                    break;
+                }
+                currentColumn++;
+            }
+
+            //act
+            reversiBoard.PlayMove(CellState.White, playCell.RowIndex, playCell.ColumnIndex);
+
+            //assert 
+            for(var column=startCell.ColumnIndex+1;column < playCell.ColumnIndex;column++)
+            {
+                var cellState = reversiBoard.m_Cells[startCell.RowIndex, column];
+                if (cellState != CellState.White)
+                {
+                    Assert.Fail("Unexpected cell state found at column {0} - {1}", column, cellState);
+                }
+            }
         }
 
         public void WhenACellIsPlayedTheCorrectOpponentCellsInAColumnSwitchColor(int startRow, int runLength, ConsoleColor playedColor)
