1fba4736044871ca7d93b0e2a92f9d1b21c95fc6
James Billings <james.billings@red-gate.com>
2016-05-17 10:14:15 +0100

    Add test case generator for horizontal/vertical rows combined

--- ReversiKata.Tests/ReversiTestCaseGenerator.cs
+++ ReversiKata.Tests/ReversiTestCaseGenerator.cs
@@ -81,6 +81,47 @@ namespace ReversiKata.Tests
             return boards;
         }
 
+        /// <summary>
+        /// Gets 5 randomly placed vertical/horizontal sets of white + n black cells
+        /// </summary>
+        /// <returns></returns>
+        internal static ReversiBoardWithStartCellInfo[] GetBoardOfMultipleCellsVerticallyAndHorizontally()
+        {
+            var boards = new ReversiBoardWithStartCellInfo[5];
+            var boardCount = 0;
+            var random = new Random();
+
+            while (boardCount < 5)
+            {
+                var startRow = random.Next(3);
+                var startColumn = random.Next(3);
+                var horizontalRunlength = random.Next(3, 7 - startColumn - 1); //make sure run length doesn't go off the board
+                var verticalRunLength = random.Next(3, 7 - startRow - 1);
+
+                ReversiBoard reversiBoard = new ReversiBoard(false);
+
+                for (var row = startRow + 1; row < startRow + verticalRunLength; row++)
+                {
+                    if (row + 1 < startRow + verticalRunLength)
+                        reversiBoard.m_Cells[row, startColumn] = CellState.Black;
+                    else
+                        reversiBoard.m_Cells[row, startColumn] = CellState.White;
+                }
+                for (var column = startColumn + 1; column < startColumn + horizontalRunlength; column++)
+                {
+                    if (column + 1 < startColumn + horizontalRunlength)
+                        reversiBoard.m_Cells[startRow, column] = CellState.Black;
+                    else
+                        reversiBoard.m_Cells[startRow, column] = CellState.White;
+                }
+
+                boards[boardCount] = new ReversiBoardWithStartCellInfo(reversiBoard, new ReversiGridCoordinate(startRow, startColumn)); 
+                boardCount++;
+            }
+
+            return boards;
+        }
+
 
     }
 }
