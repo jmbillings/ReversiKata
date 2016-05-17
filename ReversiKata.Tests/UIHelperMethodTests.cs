b27ff2ae09f81c5c7db1222d07226dd11fbf0773
James Billings <james.billings@red-gate.com>
2016-05-17 14:21:01 +0100

    Red: Add failing test for GetCellStateForColor

--- ReversiKata.Tests/UIHelperMethodTests.cs
+++ ReversiKata.Tests/UIHelperMethodTests.cs
@@ -31,5 +31,15 @@ namespace ReversiKata.Tests
         {
             Assert.AreEqual(expectedColor, m_UiHelperMethods.GetCellColorForLetter(letter));
         }
+
+        [TestCase(ConsoleColor.White, CellState.White)]
+        [TestCase(ConsoleColor.Black, CellState.Black)]
+        [TestCase(ConsoleColor.Cyan, CellState.Empty)]
+        [TestCase(ConsoleColor.DarkBlue, CellState.Empty)]
+        [TestCase(null, CellState.Empty)]
+        public void GetCellStateForColor(ConsoleColor color, CellState expectedCellState)
+        {
+            Assert.AreEqual(expectedCellState, m_UiHelperMethods.GetCellStateForColor(color));
+        }
     }
 }
