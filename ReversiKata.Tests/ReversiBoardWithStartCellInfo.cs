9d3f7b0956a37c04e47b275741bc817a3600d505
James Billings <james.billings@red-gate.com>
2016-05-17 10:13:28 +0100

    Add ReversiBoardWithStartCell class

---
+++ ReversiKata.Tests/ReversiBoardWithStartCellInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ReversiKata.Tests
+{
+    public class ReversiBoardWithStartCellInfo
+    {
+        internal ReversiBoard Board { get; private set; }
+        internal ReversiGridCoordinate StartCell { get; private set; }
+
+        public ReversiBoardWithStartCellInfo(ReversiBoard reversiBoard, ReversiGridCoordinate startCell)
+        {
+            Board = reversiBoard;
+            StartCell = startCell;
+        }
+    }
+}
