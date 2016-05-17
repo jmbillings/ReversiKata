8db2a0d60d096e46bccee8652addd77c6bd8ebf3
James Billings <james.billings@red-gate.com>
2016-05-17 14:17:09 +0100

    Red: Add failing test for GetCellColorForLetter method

---
+++ ReversiKata.Tests/UIHelperMethodTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace ReversiKata.Tests
+{
+    [TestFixture]
+    public class UIHelperMethodTests
+    {
+        private UIHelperMethods m_UiHelperMethods;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            m_UiHelperMethods = new UIHelperMethods();
+        }
+
+        [TestCase("b", ConsoleColor.Black)]
+        [TestCase("w",ConsoleColor.White)]
+        [TestCase("B", ConsoleColor.Black)]
+        [TestCase("W", ConsoleColor.White)]
+        [TestCase("a", ConsoleColor.Red)]
+        [TestCase("z", ConsoleColor.Red)]
+        [TestCase("ww", ConsoleColor.Red)]
+        [TestCase("bb", ConsoleColor.Red)]
+        public void GetColorForLetterReturnsCorrectColor(string letter, ConsoleColor expectedColor)
+        {
+            Assert.AreEqual(expectedColor, m_UiHelperMethods.GetCellColorForLetter(letter));
+        }
+    }
+}
